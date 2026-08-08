520524371c236f0776b35dbf0211c5f9b42dbb33
Twaddler01 <kenleydad@gmail.com>
2026-08-08 15:58:47 -0500

    update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import BootScene from "/scenes/BootScene.js";
-import EvolveScene from "/scenes/EvolveScene.js";
-import MainScene from "/scenes/MainScene.js";
+import BootScene from "./scenes/BootScene.js";
+import EvolveScene from "./scenes/EvolveScene.js";
+import MainScene from "./scenes/MainScene.js";
 import { DEBUG } from './config.js';
 
 // Override console.log, console.warn, and console.error for exporting into a file
