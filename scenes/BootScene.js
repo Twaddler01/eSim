d5c4cc47942395325e76725618c4f9774781445a
Twaddler01 <kenleydad@gmail.com>
2026-08-21 16:11:34 -0500

    update

--- scenes/BootScene.js
+++ scenes/BootScene.js
@@ -1,5 +1,5 @@
-//import { gameData } from '../data/gameData.js';
-//import SaveManager from '../systems/SaveManager.js';
+import { gameData } from '../data/gameData.js';
+import SaveManager from '../systems/SaveManager.js';
 
 export default class BootScene extends Phaser.Scene {
 
