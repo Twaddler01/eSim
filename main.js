5b671ebede16aea45ff12ef99ab720c5d78a63fe
Twaddler01 <kenleydad@gmail.com>
2026-08-21 15:41:43 -0500

    update

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+console.log('MAIN.JS STARTED');
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600
+};
+
+console.log('ABOUT TO START PHASER');
+
+const game = new Phaser.Game(config);
+
+console.log('PHASER STARTED');
+
+
+/*
+
 //import { DEBUG } from './config.js';
 //if (DEBUG) {
 //    import('./debug/debug.js'); // logExport, htmlExport
@@ -90,6 +107,7 @@ console.count('name');   // Count how many times something runs
 console.trace();         // Show the call stack
 */
 
+/*
 // PHASER START
 const MAX_WIDTH = 1280; // Max width for mobile portrait
 const MAX_HEIGHT = 1920; // Max height for mobile portrait
@@ -134,3 +152,4 @@ window.addEventListener("resize", () => {
     const { width, height } = getGameSize();
     game.scale.resize(width, height);
 });
+*/
