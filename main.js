066950547cb25ccb6e8ba473e90692d77b5cd57e
Twaddler01 <kenleydad@gmail.com>
2026-08-24 12:26:10 -0500

    update

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 import BootScene from './scenes/BootScene.js';
 import CreationScene from './scenes/CreationScene.js';
 
-import { DEBUG } from './config.js';
+/*import { DEBUG } from './config.js';
 if (DEBUG) {
     import('./debug/debug.js'); // logExport, htmlExport
     import('./debug/zoom.js'); // zoom
-}
-
-// OLD
-//import MainScene from './scenes/MainScene.js';
+}*/
 
 // ==================================================
 // GLOBAL GAME HELPERS
